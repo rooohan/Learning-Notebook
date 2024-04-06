ae6307e9a83f1a04a9bab8eec9c6386e8980a4ac
Neo <roohan.lee@gmail.com>
2024-04-06 13:22:03 +0800

    Add  UT

---
+++ DevOps/docker-demo/tests/app/test_hello_world.py
@@ -0,0 +1,13 @@
+import unittest
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.client = TestClient(app)
+
+    def test_root(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"message": "Hello World"})
